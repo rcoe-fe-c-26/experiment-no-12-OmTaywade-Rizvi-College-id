f0659186a9e74b9cfcf32b7b17f796816b432a24
Om Satish Taywade <omstaywade@eng.rizvi.edu.in>
2026-02-10 22:46:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,10 @@
 # five, etc.) found within the file.
 # Coder:Om Satish Taywade
 # Date:10/2/2026
-
-print("--- Extracting Words from Text File ---\n")
 file = "story.txt"
 num = int(input("Enter Length of Words: "))
 with open (file, 'r') as f:
     content = f.read().split()
     wordList = [ i for i in content if(num == len(i))]
     print(f"Following Unique words of length 8 present: {wordList}")
+
