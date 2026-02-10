b972149fd9d8e87d2bc97703680b360733fa1aae
Om Satish Taywade <omstaywade@eng.rizvi.edu.in>
2026-02-10 23:05:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@
 # five, etc.) found within the file.
 # Coder:Om Satish Taywade
 # Date:10/2/2026
-num = int(input("Enter Length of Words: "))
+num = int(input(""))
 with open ("story.txt", 'r') as f:
     content = f.read().split()
     wordList = [ i.lower() for i in content if(num == len(i))]
     wordSet = set(wordList)
     wordList2 = list(wordSet)
     print(f"{wordList2}")
+
 
